f643a8ca7b5424bd5c3e21d57ff01fd08437603b
ebrenes-dev <ebrenes@soin.co.cr>
2021-09-14 19:02:30 -0600

    application service completed

--- src/services/application.service.js
+++ src/services/application.service.js
@@ -19,4 +19,40 @@ const getAllApps = async () => {
   return result;
 };
 
-export default { getAllApps };
+const getApp = async (id) => {
+  const app = await findById(id);
+  let result = {};
+  app.rows.map((row) => {
+    result = row;
+  });
+
+  return result;
+};
+
+const createApp = async (application) => {
+  const newApp = {
+    name: String(application.name),
+    type: String(application.type),
+    created_at: new Date(),
+  };
+  await create(newApp);
+  return newApp;
+};
+
+const updateApp = async (id, application) => {
+  const newData = {
+    name: String(application.name),
+    type: String(application.type),
+  };
+  await updateById(id, newData);
+  const result = await getApp(id);
+  return Object.keys(result).length !== 0 ? result : null;
+};
+
+const removeApp = async (id) => {
+  const result = await getApp(id);
+  await removeById(id);
+  return Object.keys(result).length !== 0 ? result : null;
+};
+
+export default { getAllApps, getApp, createApp, updateApp, removeApp };
