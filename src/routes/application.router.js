a206b29597a3b07828e4449c332809270298ebd6
ebrenes-dev <ebrenes@soin.co.cr>
2021-09-14 19:03:15 -0600

    add routes

--- src/routes/application.router.js
+++ src/routes/application.router.js
@@ -3,5 +3,9 @@ import { ApplicationController } from "../controllers/controllers.js";
 const ApplicationRouter = new Router();
 
 ApplicationRouter.get("/applications", ApplicationController.getAllApps);
+ApplicationRouter.get("/applications/:id", ApplicationController.getApp);
+ApplicationRouter.put("/applications/:id", ApplicationController.updateApp);
+ApplicationRouter.delete("/applications/:id", ApplicationController.deleteApp);
+ApplicationRouter.post("/applications", ApplicationController.createApp);
 
 export { ApplicationRouter };
